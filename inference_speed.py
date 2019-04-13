ff9005b07d0297328d0b7a7c9e9ebbda467fef68
philip <philip.shurpik@gmail.com>
2019-04-13 11:16:51 +0300

    inference speed 2

--- inference_speed.py
+++ inference_speed.py
@@ -1,10 +1,15 @@
+import time
+
+import numpy as np
 import torch
 from PIL import Image
-import time
 from torchvision import models, transforms
 
 device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 image = Image.open("images/obama.jpg")
+
+init_iterations = 2
+iterations = 100
 
 normalize = transforms.Normalize(
     mean=[0.485, 0.456, 0.406],
@@ -20,7 +25,7 @@ model = models.resnet18(pretrained=True).half().eval().to(device)
 
 
 def read_image(image_path):
-    return preprocess(Image.open(image_path)).unsqueeze_(0).half().to(device)
+    return preprocess(Image.open(image_path)).half().unsqueeze_(0).to(device)
 
 
 def infer(image_batch):
@@ -28,7 +33,19 @@ def infer(image_batch):
         return model.forward(image_batch).argmax()
 
 
+def measure(image, method, name):
+    times = []
+    results = []
+    for i in range(iterations):
+        time1 = time.time()
+        result = method(image)
+        time2 = time.time()
+        times.append([time2 - time1])
+        results.append(result)
+    print(np.sum(times[init_iterations:]))
+    print(f"{name} Total speed: {1 / (np.sum(times[init_iterations:]) / (iterations - init_iterations))} FPS")
+
+
 batch_size = 16
-result = infer(read_image("images/obama.jpg").repeat(batch_size, 1, 1, 1))
-print(result)
-time.sleep(10)
+image_batch = read_image("images/obama.jpg").repeat(batch_size, 1, 1, 1)
+measure(image_batch, infer, "Half |")
